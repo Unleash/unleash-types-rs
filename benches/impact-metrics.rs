5dbce085098a610a2c7998c25b8af3d9c15baefb
Simon Hornby <liquidwicked64@gmail.com>
2025-07-10 11:32:45 +0200

    chore: wrap merge in benchmark so we can poke it with a perf stick

---
+++ benches/impact-metrics.rs
@@ -0,0 +1,65 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::{collections::HashMap, hint::black_box};
+use unleash_types::{
+    client_metrics::{ImpactMetric, ImpactMetricEnv, MetricSample, MetricType},
+    Merge,
+};
+
+fn generate_labels(start: usize, end: usize) -> HashMap<String, String> {
+    (start..end)
+        .map(|i| (format!("label_{}", i), format!("value_{}", i)))
+        .collect()
+}
+
+pub fn bench_merge(c: &mut Criterion) {
+    let first = ImpactMetricEnv {
+        app_name: "test_app".to_string(),
+        environment: "development".to_string(),
+        impact_metric: ImpactMetric {
+            name: "test_metric".to_string(),
+            help: "something".to_string(),
+            r#type: MetricType::Counter,
+            samples: vec![
+                MetricSample {
+                    value: 1.0,
+                    labels: Some(generate_labels(0, 100)),
+                },
+                MetricSample {
+                    value: 2.0,
+                    labels: Some(generate_labels(100, 200)),
+                },
+            ],
+        },
+    };
+
+    let second = ImpactMetricEnv {
+        app_name: "test_app".to_string(),
+        environment: "development".to_string(),
+        impact_metric: ImpactMetric {
+            name: "test_metric".to_string(),
+            help: "something".to_string(),
+            r#type: MetricType::Counter,
+            samples: vec![
+                MetricSample {
+                    value: 3.0,
+                    labels: Some(generate_labels(50, 150)),
+                },
+                MetricSample {
+                    value: 4.0,
+                    labels: Some(generate_labels(250, 350)),
+                },
+            ],
+        },
+    };
+
+    c.bench_function("labels_to_key", |b| {
+        b.iter(|| {
+            let first_clone = first.clone();
+            let second_clone = second.clone();
+            black_box(first_clone.merge(second_clone));
+        })
+    });
+}
+
+criterion_group!(benches, bench_merge);
+criterion_main!(benches);
