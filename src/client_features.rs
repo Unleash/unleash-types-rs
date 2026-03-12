79db2601918f04c4da7144886e7c8158f10c3d90
Simon Hornby <liquidwicked64@gmail.com>
2026-03-12 11:47:22 +0200

    feat: add semver greater than or equal and semver less than or equal

--- src/client_features.rs
+++ src/client_features.rs
@@ -49,6 +49,8 @@ pub enum Operator {
     SemverEq,
     SemverLt,
     SemverGt,
+    SemverLte,
+    SemverGte,
     RegexMatch,
     Unknown(String),
 }
@@ -197,6 +199,8 @@ impl<'de> Deserialize<'de> for Operator {
             "SEMVER_EQ" => Operator::SemverEq,
             "SEMVER_LT" => Operator::SemverLt,
             "SEMVER_GT" => Operator::SemverGt,
+            "SEMVER_LTE" => Operator::SemverLte,
+            "SEMVER_GTE" => Operator::SemverGte,
             "REGEX" => Operator::RegexMatch,
             _ => Operator::Unknown(s),
         })
